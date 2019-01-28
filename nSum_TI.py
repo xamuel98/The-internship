96e117e52eab2857db2f285a2f543125d16f2b5b
xamuel98 <46583521+xamuel98@users.noreply.github.com>
2019-01-27 16:26:43 -0800

    Add files via upload

---
+++ nSum_TI.py
@@ -0,0 +1,8 @@
+''' Program that prints 1 to n '''
+
+# Prompt the user to enter a number n
+n = int(input("Enter a number n: "))
+
+sum  = (n * (n + 1)) / 2
+
+print("The sum of numbers from 1 to ", n ,"is :", sum)
