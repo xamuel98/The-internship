87c8cc0effb1c0f3033faceecf85135d32c33d16
xamuel98 <46583521+xamuel98@users.noreply.github.com>
2019-01-27 16:27:41 -0800

    Add files via upload

---
+++ greetUser_TI.py
@@ -0,0 +1,8 @@
+''' This programs prompts users to enter their username and greets them
+ "username Welcome to Programming with Python" '''
+
+# Prompts the user to enter their user name
+username = input("Enter your username: ")
+
+# Prints the welcome message with user's name
+print(username, ", welcome to Programming with Python")
